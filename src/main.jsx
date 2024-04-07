87057f71ca759d41a35bd331589fd2de7ccb92de
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-04-07 17:05:09 +0600

    addte the secend push

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
 
 
 const router = createBrowserRouter([
   {
     path : '/',
-    element: <div>Hello from react router!!</div>
+    element: <Home></Home>,
+    children: [
+        {
+          path:'/about',
+          element: <About></About>
+        },
+        {
+          path:'/contact',
+          element:<Contact></Contact>
+        }
+    ]
   },
-  {
-    path:'/about',
-    element: <div>I am in the about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div>coll me router</div>
-  }
+ 
+
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
