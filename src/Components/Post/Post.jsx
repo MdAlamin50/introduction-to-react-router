27abab9db7940fe8fd298f4453484b67fc534a88
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-04-08 16:52:13 +0600

    added the found

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,7 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({post}) => {
-    const {id, title,body}=post
+    const {id, title,body}=post;
+
+    const navigate = useNavigate()
+
+
+    const handleShowDetail=()=>{
+        navigate(`/post/${id}`)
+    }
+
     return (
         <div className="border-2 border-red-600 rounded-lg p-10">
             <h4>{id}</h4>
@@ -9,6 +17,7 @@ const Post = ({post}) => {
             <p>{body}</p>
             <Link to={`/post/${id}`}>Post Detail</Link>
             <Link className="ml-2" to={`/post/${id}`}><button className="bg-red-600 text-white p-2 rounded-lg font-bold">Post Detail</button></Link>
+            <button onClick={handleShowDetail}>Click Naw</button>
         </div>
     );
 };
